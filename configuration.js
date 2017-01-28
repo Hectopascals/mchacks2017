301196e3206b7e567ef2ef38847b12e027fcaffa
Vincent Teng <vincent@web096193.wireless.mcgill.ca>
2017-01-28 16:59:07 -0500

    Initial commit

---
+++ configuration.js
@@ -0,0 +1,17 @@
+// Credentials
+
+var URL = 'wss://ws.dev.nuance.com/?';
+
+var APP_ID = "NMDPTRIAL_v_teng_mail_utoronto_ca20170128124910";
+var APP_KEY = "961d37652803125315af60fca0ca67509863d10ef5144f4495e3da6017f4e6ad111d133a21258d36d4ed3274b899c550fd965ef2aaad363f90109fa704855516";
+var USER_ID = "";
+var NLU_TAG = "M5030_A2328";
+
+// ASR
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var ASR_LANGUAGE = "eng-USA";
+
+// TTS
+// See: https://developer.nuance.com/public/index.php?task=supportedLanguages
+var TTS_LANGUAGE = "eng-USA";
+var TTS_VOICE = "";
